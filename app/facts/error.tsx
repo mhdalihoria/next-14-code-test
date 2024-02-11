5d4e57d22e11c0e1da25f53819e7c382674d6b8f
mhdalihoria <alihoria123456@gmail.com>
2024-02-11 12:07:27 +0200

    Adding Pic & Styling Error Page

--- app/facts/error.tsx
+++ app/facts/error.tsx
@@ -1,9 +1,26 @@
-"use client"
+"use client";
+
+import Cat from "@/public/confused-cat.svg";
+import Image from "next/image";
 
 export default function Error() {
-    return (
-        <div>
-            Something Went Wrong
-        </div>
-    )
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <Image src={Cat} alt="confused cat" width={200} />
+      <div style={{ textAlign: "center" }}>
+        <p style={{ fontSize: "2.5rem", marginBottom: "0"}}>Something Went Wrong</p>
+        <p style={{fontSize: "1.5rem"}}> Try Again Later</p>
+
+      </div>
+    </div>
+  );
 }
